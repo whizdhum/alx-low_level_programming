0ad5ba72ea8c87aa9d618e7db47948c12cb79104
Onyeka <onyekankenchor45@gmail.com>
2022-09-12 07:04:42 -0700

    task five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the phrase to proper grammar,
+ * but the outcome is a piece of art.
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
