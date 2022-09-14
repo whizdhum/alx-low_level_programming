d1d4873c37575ea7be82205b3b94576aa8d5efc3
Onyeka <onyekankenchor45@gmail.com>
2022-09-13 20:59:15 -0700

    Task eight

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,8 @@ void jack_bauer(void)
 	{
 	while (minutes <= 59)
 	{
-	minutes_remainder = minutes %10;
-	hours_remainder = hours %10;
+	minutes_remainder = minutes % 10;
+	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
 	_putchar(':');
