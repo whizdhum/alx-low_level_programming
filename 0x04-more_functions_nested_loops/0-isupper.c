ecd0023922086c73690ec93b44187d0021e9ccb3
Onyeka <onyekankenchor45@gmail.com>
2022-09-18 16:49:01 -0700

    task zero

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,12 @@
 
 int _isupper(int c)
 {
-  if (c >= 65 && c < 91)
-  {
-    return (1);
-  }
-  else
-  {
-    return (0);
-  }
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
