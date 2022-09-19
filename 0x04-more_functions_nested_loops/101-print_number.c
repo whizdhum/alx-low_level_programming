dd7c1ee01706b224d0cf9afeffd973ae28430349
Onyeka <onyekankenchor45@gmail.com>
2022-09-19 02:28:08 -0700

    tasks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,27 +6,28 @@
  */
 void print_number(int n)
 {
-  unsigned int i, j, count;
+	unsigned int i, j, count;
 
-  if (n < 0)
-  {
-    _putchar(45);
-    i = n * -1;
-  }
-  else
-  {
-    i = n;
-  }
-  j = i;
-  count = 1;
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+	j = i;
+	count = 1;
 
-  while (j > 9)
-  {
-    j /= 10;
-    count *= 10;
-  }
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
 
-  for (; count >= 1; count /= 10)
-  {
-    _putchar (((i / count) % 10) + 48);
-  }
+	for (; count >= 1; count /= 10)
+	{
+		_putchar (((i / count) % 10) + 48);
+	}
+}
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,22 +7,23 @@
 
 void print_diagonal(int n)
 {
-  int i = 0;
-  int j;
+	int i = 0;
+	int j;
 
-  if (n > 0)
-  {
-    for (; i < n; i++)
-    {
-      for (j = 0; j < i; j++)
-      {
-        _putchar(' ');
-      }
-      _putchar(92);
-      _putchar('\n');
-    }
-  }
-  else
-  {
-    _putchar('\n');
-  }
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
